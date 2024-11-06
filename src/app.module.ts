cb15124e4a62496a5abe5bc948de415472afc466
Rodrigo Chiong <letsan.chr@gmail.com>
2024-11-05 21:57:30 -0300

    Add blank DialogsService

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersService } from './users/users.service';
 import { UsersController } from './users/users.controller';
+import { DialogsService } from './dialogs/dialogs.service';
 
 @Module({
   imports: [],
   controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  providers: [AppService, UsersService, DialogsService],
 })
 export class AppModule {}
